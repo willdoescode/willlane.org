49798215e21e056fa7fabd8605d7c6bc8c8af9c9
Will Lane <williamlane923@gmail.com>
2021-12-09 09:21:37 -0800

    quote stuff

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 
 import styles from '../styles/Quote.module.css'
 
@@ -8,23 +8,11 @@ interface Quote {
   content: string;
 }
 
-const Quote: NextPage = () => {
-  const [quote, setQuote] = useState<Quote>();
-  const [isLoading, setLoading] = useState(false);
-  
-  useEffect(() => {
-    setLoading(true);
-    fetch('https://api.quotable.io/random?maxLength=100')
-      .then(res => res.json())
-      .then(data => { 
-        setQuote(data); 
-        setLoading(false) 
-      });
-  }, [])
+export interface QuoteProps {
+  quote: Quote;
+}
 
-  if (isLoading) return <h1>Loading...</h1>;
-  if (!quote) return <h1>Error</h1>;
-
+const Quote: NextPage<QuoteProps> = ({quote}: QuoteProps) => {
   return (
     <>
       <h1 className={styles.content}>{quote.content}</h1>
@@ -32,5 +20,7 @@ const Quote: NextPage = () => {
     </>
   )
 }
+
+
 
 export default Quote;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,26 @@
 import type { NextPage } from 'next';
+import type { QuoteProps } from '../../components/Quote';
 
 import Header from '../components/Header';
 import Quote from '../components/Quote';
 import styles from '../styles/Home.module.css';
 
-const Index: NextPage = () => {
+
+const Index: NextPage = ({quote}: QuoteProps) => {
   return (
     <>
       <Header />
-      <Quote />
+      <Quote quote={quote} />
     </>
   )
 }
+
+Index.getInitialProps = async (): Promise<QuoteProps> => {
+  const res = await fetch('https://api.quotable.io/random?maxLength=100');
+  const quote: Quote = await res.json();
+
+  return { quote };
+}
+
 
 export default Index;
